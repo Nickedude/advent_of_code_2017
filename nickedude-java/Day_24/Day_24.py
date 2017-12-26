e44e0dfde1a0d648154fe8cef8a05edf35afb841
Nickedude <niklas.erik.gustafson@gmail.com>
2017-12-26 19:20:32 +0100

    Day 24

--- nickedude-java/Day_24/Day_24.py
+++ nickedude-java/Day_24/Day_24.py
@@ -33,7 +33,36 @@ def findMaxStrength(connection, connectionsToPorts, used, size):
 
     return sizeToReturn
 
+def findMaxStrengthAndLength(connection, connectionsToPorts, used, strength, longestStrength):
+    listOfPorts = connectionsToPorts[connection]
+    strengthToReturn = strength
+    longestStrengthToReturn = longestStrength
+    lengthToReturn = len(used)
 
+    for i in range(0,len(listOfPorts)):
+        p = listOfPorts[i]
+
+        if not p in used:
+            newUsed = used.copy()
+            newUsed.add(p)
+            newStrength = strength + p.inp + p.outp
+            newLongestStrength = strength + p.inp + p.outp
+            k = 0
+            if p.inp == connection:
+                (k, p, l) = findMaxStrengthAndLength(p.outp, connectionsToPorts, newUsed, newStrength, newLongestStrength)
+            elif p.outp == connection:
+                (k, p, l) = findMaxStrengthAndLength(p.inp, connectionsToPorts, newUsed, newStrength, newLongestStrength)
+            else:
+                print("Something's wrong")
+
+            if k > strengthToReturn:
+                strengthToReturn = k
+
+            if l > lengthToReturn:
+                lengthToReturn = l
+                longestStrengthToReturn = p
+
+    return (strengthToReturn, longestStrengthToReturn, lengthToReturn)
 
 inputFile = open('Day_24.txt')
 lines = inputFile.readlines()
@@ -67,17 +96,19 @@ for i in range(0,len(ports)):
         connectionsToPorts[outp].append(ports[i])
 
 maximum = 0
+strengthOfLongest = 0
 for i in range(0,len(starters)):
-    print("Running: " + str(starters[i]))
     s = set()
     s.add(starters[i])
     if starters[i].inp == 0:
-        k = findMaxStrength(starters[i].outp, connectionsToPorts, s, starters[i].outp)
+        (a, b, dontCare) = findMaxStrengthAndLength(starters[i].outp, connectionsToPorts, s, starters[i].outp, starters[i].outp)
     else:
-        k = findMaxStrength(starters[i].inp, connectionsToPorts, s, starters[i].inp)
+        (a, b, dontCare) = findMaxStrengthAndLength(starters[i].inp, connectionsToPorts, s, starters[i].inp, starters[i].inp)
 
-    if k > maximum:
-        maximum = k
+    if a > maximum:
+        maximum = a
+    if b > strengthOfLongest:
+        strengthOfLongest = b
 
-print()
-print(maximum)
+print("Max strength: " + str(maximum))
+print("Strength of longest: " + str(strengthOfLongest))
